f961c4c22cb696401fdf58898d265965b0acff53
J R M <jwrmmine@protonmail.com>
2025-06-15 14:03:27 +0200

    Update captains-log.spec.js
    Added checks for randomness

--- captains-log.spec.js
+++ captains-log.spec.js
@@ -6,12 +6,18 @@ describe('randomShipRegistryNumber',() => {
     expect(randomShipRegistryNumber().test(/NCC-[1-9][0-9]{3}/)).toBe(true);
     expect(randomShipRegistryNumber().test(/NCC-[1-9][0-9]{3}/)).toBe(true);
   });
+  test('returns a random registry number',() => {
+    expect(randomShipRegistryNumber()).not.toEqual(randomShipRegistryNumber()) 
+  });
 });
 describe('randomStardate',() => {
   test('stardates are valid',() => {
     expect(41000<=randomStardate()<42000).toBe(true);
     expect(41000<=randomStardate()<42000).toBe(true);
     expect(41000<=randomStardate()<42000).toBe(true);
+  });
+  test('returns a random stardate',() => {
+    expect(randomStardate()).not.toEqual(randomStardate()) 
   });
 });
 describe('randomPlanetClass', () => {
@@ -20,5 +26,8 @@ describe('randomPlanetClass', () => {
     expect(planetClasses.includes(randomPlanetClass())).toBe(true)
     expect(planetClasses.includes(randomPlanetClass())).toBe(true)
     expect(planetClasses.includes(randomPlanetClass())).toBe(true)
+  });
+  test('returns a random planet class',() => {
+    expect(randomPlanetClass()).not.toEqual(randomPlanetClass())
   });
 });
