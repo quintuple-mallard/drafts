6be0892313ababedc81a2ea7bb850a9c504a29b2
J R M <jwrmmine@protonmail.com>
2025-06-15 13:32:23 +0200

    Update captains-log.spec.js

--- captains-log.spec.js
+++ captains-log.spec.js
@@ -1,2 +1,24 @@
 import { describe, expect, test } from '@jest/globals';
 import { randomShipRegistryNumber, randomStardate, randomPlanetClass } from './captains-log';
+describe('randomShipRegistryNumber',() => {
+  test('registry numbers are valid',() => {
+    expect(randomShipRegistryNumber().test(/NCC-[1-9][0-9]{3}/)).toBe(true);
+    expect(randomShipRegistryNumber().test(/NCC-[1-9][0-9]{3}/)).toBe(true);
+    expect(randomShipRegistryNumber().test(/NCC-[1-9][0-9]{3}/)).toBe(true);
+  });
+});
+describe('randomStardate',() => {
+  test('stardates are valid',() => {
+    expect(41000<=randomStardate()<42000).toBe(true);
+    expect(41000<=randomStardate()<42000).toBe(true);
+    expect(41000<=randomStardate()<42000).toBe(true);
+  });
+});
+describe('randomPlanetClass', () => {
+  test('planet classes are valid',  () => {
+    const planetClasses = ['D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'];
+    expect(planetClasses.includes(randomPlanetClass())).toBe(true)
+    expect(planetClasses.includes(randomPlanetClass())).toBe(true)
+    expect(planetClasses.includes(randomPlanetClass())).toBe(true)
+  });
+});
