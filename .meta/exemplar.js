92e6ed31cb3f54414b418f65d99db1f1df5b44ec
J R M <jwrmmine@protonmail.com>
2025-06-15 10:37:04 +0200

    Update exemplar.js

--- .meta/exemplar.js
+++ .meta/exemplar.js
@@ -1,10 +1,10 @@
-function randomShipRegistryNumber() {
+export function randomShipRegistryNumber() {
   return `NCC-${Math.floor(1000 + Math.random()*8000)}`
 }
-function randomStardate() {
+export function randomStardate() {
   return 41000 + Math.random() * 1000
 }
-function randomPlanetClass() {
+export function randomPlanetClass() {
   const planetClasses = ['D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y']
   return [Math.floor(Math.random() * 10)]
 }
